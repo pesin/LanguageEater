0d35db9bd501481da3ca872a86cf0a753c7f350e
Pesin <somemouse@gmail.com>
2016-07-04 11:59:23 -0400

    going on

---
+++ LanguageEater.Lib/GoGetter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+
+namespace LanguageEater.Lib
+{
+    class GoGetter
+    {
+        private static string workDir = System.Configuration.ConfigurationManager.AppSettings["workDir"];
+        private static string linksFile = System.Configuration.ConfigurationManager.AppSettings["linksFile"];
+
+        public static void GoGetThem()
+        {
+            XDocument doc = XDocument.Load(linksFile);
+
+            
+
+            foreach (var el in doc.Elements("a"))
+            {
+                string url = el.Attribute("href").Value;
+                string filename = url.Substring(url.LastIndexOf("/") + 1);
+
+                bool flag = DownloadFile(url, filename);
+                if (flag)
+                {
+                    //unzip
+                    //load to db
+                }
+            }
+        }
+
+        private static bool DownloadFile(string url, string filename)
+        {
+            bool retval = true;
+            var request = WebRequest.CreateHttp(url);
+            request.AllowReadStreamBuffering = true;
+            FileStream f=null;
+            Stream responseStream = null;
+            try
+            {
+                var response = request.GetResponse();
+                 responseStream = response.GetResponseStream();
+                 f = File.Create(string.Format("{0}\\{1}", workDir, filename));
+
+                while (responseStream.CanRead)
+                {
+                    f.WriteByte((byte)responseStream.ReadByte());
+                }
+                f.Flush();
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.ToString());
+                retval = false;
+            }
+            finally
+            {
+                if (f != null)
+                {
+                    f.Close();
+                    f.Dispose();
+                }
+
+                if (responseStream != null)
+                {
+                    responseStream.Close();
+                    responseStream.Dispose();
+                }
+            }
+            return retval;
+        }
+    }
+}
--- LanguageEater.Lib/MongoMongo.cs
+++ LanguageEater.Lib/MongoMongo.cs
@@ -2,10 +2,12 @@
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace LanguageEater.Lib
 {
@@ -14,13 +16,47 @@ namespace LanguageEater.Lib
     {
         private static MongoClient client = new MongoClient(System.Configuration.ConfigurationManager.ConnectionStrings["mongo"].ConnectionString);
 
+
+        private static BsonDocument getNGramDocOnly(FiveGram ngram)
+        {
+            var document = BsonDocument.Create(ngram);
+            document.Remove("Count");
+
+            return document;
+        }
+
         private void insert5Gram(FiveGram ngram)
         {
             
             var database = client.GetDatabase("english5");
             var collection = database.GetCollection<BsonDocument>("corpus");
             var document =  BsonDocument.Create(ngram);
-            collection.InsertOneAsync(document);
+
+            //     var filter = new BsonDocumentFilterDefinition.Eq("i", 71);
+
+            //   var newDoc = new BsonDocument { { "_id", 123 }, { "someKey", "someValue" } };
+
+            var filterObj = new MongoDB.Driver.BsonDocumentFilterDefinition<BsonDocument>(getNGramDocOnly(ngram));
+            var updateObj = new BsonDocument("$inc", new BsonDocument { { "Count", ngram.Count } });
+           
+          var newDoc= collection.FindOneAndUpdate<BsonDocument>(
+               filter:filterObj,
+               update: updateObj,
+               options:new FindOneAndUpdateOptions<BsonDocument, BsonDocument>() { ReturnDocument = ReturnDocument.After });
+
+            Console.WriteLine(newDoc);
+
+            //collection.FindOneAndUpdate(
+            //    filter: filterObj,
+            //    options: new UpdateOptions { IsUpsert = true },
+            //    update: Update.Inc()
+            //    //ndModify(Queue, SortBy.Null, Update.Inc("MessageCount", 1), true).GetModifiedDocumentAs<UserDocument>();
+
+            // collection.UpdateOne(
+            //       filter: ,
+            //      options: new UpdateOptions { IsUpsert = true },
+            //      update: document);
+            //  collection.InsertOneAsync(document);
 
             return;
 
@@ -38,23 +74,22 @@ namespace LanguageEater.Lib
             };*/
 
           
-            // generate 100 documents with a counter ranging from 0 - 99
-            var documents = Enumerable.Range(0, 100).Select(i => new BsonDocument("counter", i));
-            //collection.InsertMany(documents);
-             collection.InsertManyAsync(documents);
+            //// generate 100 documents with a counter ranging from 0 - 99
+            //var documents = Enumerable.Range(0, 100).Select(i => new BsonDocument("counter", i));
+            ////collection.InsertMany(documents);
+            // collection.InsertManyAsync(documents);
 
-            var count = collection.Count(new BsonDocument());
+            //var count = collection.Count(new BsonDocument());
 
-            var filter = Builders<BsonDocument>.Filter.Eq("i", 71);
-             document = collection.Find(filter).First();
-            Console.WriteLine(document);
+            // document = collection.Find(filter).First();
+            //Console.WriteLine(document);
 
-             filter = Builders<BsonDocument>.Filter.Gt("i", 50);
-            var cursor = collection.Find(filter).ToCursor();
-            foreach (var d in cursor.ToEnumerable())
-            {
-                Console.WriteLine(d);
-            }
+            // filter = Builders<BsonDocument>.Filter.Gt("i", 50);
+            //var cursor = collection.Find(filter).ToCursor();
+            //foreach (var d in cursor.ToEnumerable())
+            //{
+            //    Console.WriteLine(d);
+            //}
         }
     }
 }
